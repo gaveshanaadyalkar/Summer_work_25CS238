5681fe516fd91a47ce08462cae2b15ce2fd27e78
gaveshanaadyalkar <gaveshanaadyalkar@gmail.com>
2026-06-23 17:23:40 +0530

    d-17 uplodaded.

---
+++ d-17/1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+
+    int arr1[]={1,2,3,4,5};
+    int arr2[]={6,7,8,9};
+    int size1=5;
+    int size2=4;
+    int merge[9];
+    int index=0;
+    for(int i=0;i<size1;i++)
+    {
+     merge[index]=arr1[i];
+     index++;
+    }
+    for(int i=0;i<size2;i++)
+    {
+        merge[index]=arr2[i];
+        index++;
+    }
+    printf("merged array");
+    for(int i=0;i<index;i++)
+    {
+        printf("%d",merge[i]);
+    }
+
+    return 0;
+}
---
+++ d-17/2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+
+
+        int arr1[]={1,2,3,4};
+        int arr2[]={3,4,5,6};
+        int size1=4;
+        int size2=4;
+        printf("union");
+        for(int i=0;i<size1;i++)
+        {
+            printf("%d",arr1[i]);
+        }
+        for(int i=0;i<size2;i++)
+        {
+            int isDouble=0;
+            for(int j=0;j<size1;j++)
+            {
+                if(arr2[i]==arr1[j])
+                {
+                    isDouble=1;
+                    break;
+                }
+            }
+            if(isDouble==0)
+            {
+                printf("%d",arr2[i]);
+            }
+        }
+    printf("\n");
+    return 0;
+
+
+}
